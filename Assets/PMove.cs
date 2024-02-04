764b715a172719c511951c6ee94e8bb5b760172b
dotker225 <158827152+dotker225@users.noreply.github.com>
2024-02-04 17:43:14 +0300

    0.13

--- Assets/PMove.cs
+++ Assets/PMove.cs
@@ -2,9 +2,12 @@ using UnityEngine;
 
 public class PMove : MonoBehaviour
 {
+    [Header("Передвижение")]
     [SerializeField] private Vector2 _direction;
     [SerializeField] private float _speed;
+    [SerializeField] private bool _isLookToRight = true;
     private Rigidbody2D _rb2d;
+    private float _directionX;
     private void Start()
     {
         _rb2d = GetComponent<Rigidbody2D>();
@@ -12,14 +15,18 @@ public class PMove : MonoBehaviour
     private void Update()
     {
         //transform.Translate(_direction * Time.deltaTime);
+        transform.localScale = new Vector2(_directionX,1);
+
     }
 
     private void FixedUpdate()
     {
         //transform.Translate(_direction);
-        var directionX = Input.GetAxis("Horizontal");
-        Debug.Log(directionX);
-        var direction = new Vector2(directionX, 0);
-        _rb2d.AddForce(direction * _speed);
+        _directionX = Input.GetAxis("Horizontal");
+        var directionY = Input.GetAxis("Vertical");
+        //Debug.Log(directionX);
+        _direction = new Vector2(_directionX, directionY);
+        //_rb2d.AddForce(direction * _speed);
+        _rb2d.velocity = _direction * _speed;
     }
 }
